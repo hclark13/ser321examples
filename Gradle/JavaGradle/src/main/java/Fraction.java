81febbc8bc1a8ab207138dce6d775dac61ea61a9
hclark13 <hclark13@asu.edu>
2023-10-19 01:51:36 -0600

    Edit to accept 2 args

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -3,58 +3,35 @@ import java.io.*;
  * Purpose: demonstrate simple Java Fraction class with command line,
  * jdb debugging, and Ant build file.
  *
- * Ser321 Foundations of Distributed Applications
- * see http://pooh.poly.asu.edu/Ser321
- * @author Tim Lindquist Tim.Lindquist@asu.edu
- *         Software Engineering, CIDSE, IAFSE, ASU Poly
- * @version January 2020
+ * Updated for SER321 Fall B Assignment 1: Change Fraction.main so it accepts 2 arguments
+ *
+ * @author Hayden Clark (hclark13)
+ * @version October 2023
  */
 public class Fraction {
+ public static void main (String args[]) {
+        int num = 0;
+        int denom = 0;
 
-   private int numerator, denominator;
+	if (args.length == 2) {
+       	    //int num = 0;
+       	    //int denom = 0;
+            try {
+              num = Integer.parseInt(args[0]);
+              denom = Integer.parseInt(args[1]);
+            } catch (Exception e) {
+              System.out.println("Arguments: " + args[0] + ", " + args[1] + " must be integers.");
+              System.exit(1);
+            }
+            System.out.println(num + " / " + denom + " = " + num / denom);
+        } else if (args.length == 1) {
+	    //chek which arg is empty and fill with default value
+	    
 
-   public Fraction(){
-      numerator = denominator = 0;
-   }
-
-   public void print() {
-    System.out.print(numerator + "/" + denominator );
-   }
-
-   public void setNumerator (int n ){
-      numerator = n;
-   }
-
-   public void setDenominator (int d) {
-      denominator = d;
-   }
-
-   public int getDenominator() {
-      return denominator;
-   }
-
-   public int getNumerator() {
-      return numerator;
-   }
-
-   public static void main (String args[]) {
-      try {
-         // create a new instance
-         // Fraction *frac = [[Fraction alloc] init];
-         Fraction frac = new Fraction();
-
-         // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
-
-         // print it
-         System.out.print("The fraction is: ");
-         frac.print();
-         System.out.println("");
-
-      }catch(Exception e) {
-         e.printStackTrace();
-      }
-   }
+            System.out.println(num + " / " + denom + " = " + num / denom);
+        } else { //no args, prints default values
+		System.out.println(num + " / " + denom + " = " + num / denom);
+        }
+  }
 }
 
